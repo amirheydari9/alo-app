3c02b6880c06041dbb7147636b6ceeae46fefb5b
amir <amirheydari9@gmail.com>
2021-04-22 01:04:38 -0700

    add material module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
+
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+
   {
     path: 'products',
     loadChildren: () => import('./views/products/product.module').then(m => m.ProductModule),
--- src/app/views/products/product.module.ts
+++ src/app/views/products/product.module.ts
@@ -4,6 +4,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ProductsComponent} from './products.component';
 import {ProductsRoutingModule} from './products-routing.module';
+import {ProductsMaterialModule} from '../../shared/modules/material/products-material.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {ProductsRoutingModule} from './products-routing.module';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ProductsMaterialModule
   ],
   providers: [],
 })
